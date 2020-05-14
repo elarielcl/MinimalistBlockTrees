aa7f34df712aea9464748e13e5e30a5e0614e912
Ariel <el.ariel.cl@gmail.com>
2020-05-15 00:18:33 +0300

    Smthg

---
+++ tests/GTestFixture.cpp
@@ -0,0 +1,23 @@
+
+#include "gtest/gtest.h"
+
+
+
+class GTestFixture : public ::testing::Test {
+protected:
+    virtual void TearDown() {
+
+    }
+
+    virtual void SetUp() {
+
+    }
+
+public:
+
+    GTestFixture() {
+    }
+
+    virtual ~GTestFixture() {
+    }
+};
